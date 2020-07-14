f226d6b18372f039c880cf9535f23cf3bbf522bb
Jofiel Batista <jofielbatista@hotmail.com>
2020-07-14 12:23:33 -0600

    Search in a Binary Search Tree (36/36)
    Given the root node of a binary search tree (BST) and a value.
    You need to find the node in the BST that the node's value equals the
    given value. Return the subtree rooted with that node. If such node
    doesn't exist, you should return NULL.

---
+++ src/BinaryTreeAlgorithms/BTSSearch.java
@@ -0,0 +1,36 @@
+package BinaryTreeAlgorithms;
+
+public class BTSSearch {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode node = find(root, val);
+        return node;
+    }
+
+    private TreeNode find(TreeNode node, int val){
+        if(node == null) return null;
+        if(node.val == val) return node;
+        if(node.val > val) return find(node.left, val);
+        if(node.val < val) return find(node.right, val);
+        return node;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
