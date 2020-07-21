6b77fdbb920df78a6c11c05a7f9da50c49755425
Jofiel Batista <jofielbatista@hotmail.com>
2020-07-21 16:13:43 -0600

    Kth Largest Element in a Stream (10/10)
    Design a class to find the kth largest element in a stream. Note that
    it is the kth largest element in the sorted order, not the kth
    distinct element.
    Your KthLargest class will have a constructor which accepts an integer
    k and an integer array nums, which contains initial elements from the
    stream. For each call to the method KthLargest.add, return the element
    representing the kth largest element in the stream.
    
    NOTE: I resolved the algorith using an ArrayList, I have to improve
          it usign a binary tree.

---
+++ src/BinaryTreeAlgorithms/KthLargest.java
@@ -0,0 +1,30 @@
+package BinaryTreeAlgorithms;
+
+import java.util.*;
+
+public class KthLargest {
+
+    ArrayList<Integer> sortedList = new ArrayList<>();
+    int index;
+
+    public KthLargest(int k, int[] nums) {
+        index = k;
+
+        for (int current : nums) {
+            sortedList.add(current);
+        }
+
+        Collections.sort(sortedList);
+    }
+
+    public int add(int val) { 
+        int position = 0;
+        int size = sortedList.size();
+        while (position < size && val > sortedList.get(position)){
+            position++;
+        }
+        sortedList.add(position, val);
+        return sortedList.get(sortedList.size() - index);
+    }
+
+}
