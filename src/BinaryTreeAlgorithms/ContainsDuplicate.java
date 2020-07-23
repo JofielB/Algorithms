a337c9897b114aed2847399676f4a01eaaa661d3
Jofiel Batista <jofielbatista@hotmail.com>
2020-07-23 14:43:32 -0600

    Contains Duplicate III (41/41)
    Given an array of integers, find out whether there are two distinct
    indices i and j in the array such that the absolute difference between
    nums[i] and nums[j] is at most t and the absolute difference between
    i and j is at most k.

---
+++ src/BinaryTreeAlgorithms/ContainsDuplicate.java
@@ -0,0 +1,35 @@
+package BinaryTreeAlgorithms;
+
+public class ContainsDuplicate {
+
+    public  boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        long[] arr = new long[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i];
+        }
+        return containsNearbyAlmostDuplicateLong(arr, k, t);
+    }
+
+    public  boolean containsNearbyAlmostDuplicateLong(long[] nums, long k, long t) {
+        if (nums.length < 2) return false;
+        if (k == 0 && t != 0) return false;
+        if (k == 0 && t == 0) return true;
+
+        for (int i = 0, j = 1; i < nums.length - 1; j++) {
+            if (j >= nums.length) return false;
+
+            long val1 = nums[i] - nums[j] < 0 ? (nums[i] - nums[j]) * -1 : nums[i] - nums[j];
+
+            if (val1 <= t) return true;
+
+            long val2 = (i - j) * -1;
+
+            if (val2 >= k || j + 1 >= nums.length) {
+                i++;
+                j = i;
+            }
+        }
+
+        return false;
+    }
+}
