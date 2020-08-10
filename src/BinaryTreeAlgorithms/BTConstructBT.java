4be097b9b37b5351bcc720b8d8ec1550cae39f03
Jofiel Batista <jofielbatista@hotmail.com>
2020-08-10 17:12:50 -0600

    Construct Binary Tree from Inorder and Postorder Traversal
    Create the algorithm class

---
+++ src/BinaryTreeAlgorithms/BTConstructBT.java
@@ -0,0 +1,26 @@
+package BinaryTreeAlgorithms;
+
+public class BTConstructBT {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
