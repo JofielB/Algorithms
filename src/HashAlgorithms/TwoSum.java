e5495bf1bfc0a769f309733a8b1a9cc6bb6f4db7
Jofiel Batista <jofielbatista@hotmail.com>
2020-09-09 14:24:54 -0600

    Two Sum algorithm improved
    Add a better solution using Maps

--- src/HashAlgorithms/TwoSum.java
+++ src/HashAlgorithms/TwoSum.java
@@ -1,5 +1,7 @@
 package HashAlgorithms;
 
+import java.util.HashMap;
+
 public class TwoSum {
     //Brute force
     public int[] twoSum(int[] nums, int target) {
@@ -19,4 +21,23 @@ public class TwoSum {
         return numbers;
     }
 
+    //A better approach
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer>  valuesMap = new HashMap<>();
+        int size = nums.length;
+
+        for (int i = 0; i <size ; i++) {
+            valuesMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i <size ; i++) {
+            int remaining = target - nums[i];
+            if(valuesMap.containsKey(remaining) && valuesMap.get(remaining) != i){
+                return new int[] {i, valuesMap.get(remaining)};
+            }
+        }
+
+        throw new IllegalArgumentException("No solution");
+    }
+    
 }
